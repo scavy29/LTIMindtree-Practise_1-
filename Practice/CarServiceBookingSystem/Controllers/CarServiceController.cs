9101d1a7f538923b8dd5d61b3081c5b8bd8653f3
Iamneo <techadmin@iamneo.ai>
2023-11-01 11:31:32 +0000

    Backup Commit

---
+++ Practice/CarServiceBookingSystem/Controllers/CarServiceController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CarServiceBookingSystem.Models;
+
+namespace CarServiceBookingSystem.Controllers
+{
+    // [Route("[controller]")]
+    public class CarServiceController : Controller
+    {
+        CarServiceDbContext db=new CarServiceDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CarService cs)
+        {
+            db.CarServices.Add(cs);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
