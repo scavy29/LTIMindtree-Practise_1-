599b49e9cca65a86d31bbb171136365c627252ae
Iamneo <techadmin@iamneo.ai>
2023-11-02 07:46:55 +0000

    Backup Commit

--- Practice/BlogApp/Models/Post.cs
+++ Practice/BlogApp/Models/Post.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlogApp.Models
 {
@@ -9,7 +10,12 @@ namespace BlogApp.Models
     {
         // [HasNoKey]
         public int Id{set;get;}
+
+        [Required]
         public string? Title{set;get;}
+
+        [Required]
+        [MinLength(10)]
         public string? Content{set;get;}
         public string? Author{set;get;}
         public DateTime PublishDate{set;get;}
