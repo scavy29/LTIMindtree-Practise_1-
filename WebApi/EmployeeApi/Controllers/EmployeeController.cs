cde1e49cb877d565ff1dec441b71d3ecfb2c4352
Iamneo <techadmin@iamneo.ai>
2023-11-03 05:27:58 +0000

    Backup Commit

--- WebApi/EmployeeApi/Controllers/EmployeeController.cs
+++ WebApi/EmployeeApi/Controllers/EmployeeController.cs
@@ -8,23 +8,40 @@ using EmployeeApi.Models;
 namespace EmployeeApi.Controllers
 {
 
-    //Declaring a List to store list of employees
-    private List<Employee> employeelist=new List<Employee>{
-        new Employee{EmployeeId=101,EmployeeName="Vikrant",Salary=20400},
-        new Employee{EmployeeId=121,EmployeeName="Sushil",Salary=50900},
-        new Employee{EmployeeId=131,EmployeeName="Harshal",Salary=45330},
-        new Employee{EmployeeId=141,EmployeeName="Must",Salary=45000},
-        new Employee{EmployeeId=151,EmployeeName="Ankit",Salary=21400},
-    };
+
     [ApiController]
+
+    //Follow the link in this format
     [Route("api/[controller]")]
 
     public class EmployeeController : ControllerBase
     {
+        //Declaring a List to store list of employees
+        private List<Employee> employeelist=new List<Employee>{
+            new Employee{EmployeeId=101,EmployeeName="Vikrant",Salary=20400},
+            new Employee{EmployeeId=121,EmployeeName="Sushil",Salary=50900},
+            new Employee{EmployeeId=131,EmployeeName="Harshal",Salary=45330},
+            new Employee{EmployeeId=141,EmployeeName="Musu",Salary=45000},
+            new Employee{EmployeeId=151,EmployeeName="Ankit",Salary=21400}
+        };
+    
         //To Iterate the Emp List & to get all details as well(***)
-        public IEnumerable<EmployeeApi> Get()
+        public IEnumerable<Employee> Get()
         {
             return (employeelist);
+        }
+
+        [Route("{id}")]
+        public Employee Get(int id)
+        {
+            var emp = employeelist.Find(e=>e.EmployeeId==id);
+            return emp;
+        }
+
+        public void Post(Employee emp)
+        {
+            employeelist.Add(emp);
+            return
         }
     }
 }
