2769a3e9d47fc439556cc7fdc8b725cd4cd9f8a1
Iamneo <techadmin@iamneo.ai>
2023-11-08 06:09:03 +0000

    Backup Commit

---
+++ DemoSolutions/DemoSolutions.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DemoSolutions.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
