c2fdc7ea752c0a6fb387c61ae2a86ab815c1e8a4
Iamneo <techadmin@iamneo.ai>
2023-11-08 04:14:59 +0000

    Backup Commit

---
+++ SampleProject/demo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleProject
+{
+    public class demo
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Hello BC");
+        }
+    }
+}
