d8b78247eeaaf5cccdbcd28f769d076327503e43
Iamneo <techadmin@iamneo.ai>
2023-11-09 04:28:48 +0000

    Backup Commit

--- Angular/demo.ts
+++ Angular/demo.ts
@@ -11,6 +11,35 @@ let know:unknown=34;
 //Any can store almost anything
 let go:any="de"
 
-go.IfFixe();
 
-console.log(isOk);
+let tuple:[string,number]=["string",34,54,23];
+
+let names:string[] | number[] = ["Vikrant","Sushil","Harshal","Anjali"];
+
+let anyarray:any=["Radha",21,true,93.27];
+
+console.log(names[0]);
+
+console.log(tuple[0]);
+console.log(anyarray[3]);
+
+
+let anonymousfunc=function(x:number,y:number):number
+{
+    return x+y;
+}
+
+console.log(anonymousfunc(5,4));
+
+
+let lambda=(x:number,y:number):number=>x+y;
+
+console.log(lambda(5,10));
+
+
+function Demo(x:number,y:number=10)
+{
+    return x+y;
+}
+
+console.log(Demo(4));
