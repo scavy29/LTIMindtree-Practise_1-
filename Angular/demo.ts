3387cc9f386c414ab76be69ffdb80ec43f5f858b
Iamneo <techadmin@iamneo.ai>
2023-11-09 04:13:50 +0000

    Backup Commit

---
+++ Angular/demo.ts
@@ -0,0 +1,16 @@
+let nme:string="demo";
+
+let num:number=24.56;
+
+let isOk:boolean=true;
+
+let unionType:string | number | boolean = true;
+
+let know:unknown=34;
+
+//Any can store almost anything
+let go:any="de"
+
+go.IfFixe();
+
+console.log(isOk);
